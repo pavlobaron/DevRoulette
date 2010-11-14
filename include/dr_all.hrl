5b2dceebf196f28228a2d3b0166f5477f30acc2e
pavlobaron <pb@pbit.org>
2010-11-14 09:23:01 +0100

    refactored records / macros into a separate include

---
+++ include/dr_all.hrl
@@ -0,0 +1,9 @@
+%% @author Pavlo Baron <pb@pbit.org>
+%% @doc This is the header including all records and macros of DevRoulette.
+%% @copyright 2010 Pavlo Baron
+
+-record(state, {
+	  start,
+	  player1,
+	  player2
+        }).
